bee67084ce293985be2902bb4794aec4a8c32784
luarodriguess <lua.rodrigues@lydia.com.br>
2025-09-03 21:37:02 -0300

    fix: db use .env

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -4,12 +4,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: "postgresql://postgres:Cleiton2024@46.202.147.67:5434/procedure_control"
-    }
-  }
-})
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
